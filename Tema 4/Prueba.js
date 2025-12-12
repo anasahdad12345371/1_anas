af4133aec39e17fc5f0ebf4d18863dff38852b4d
anasahdad12345371 <anas.ahdad@educa.madrid.org>
2025-12-12 14:20:01 +0100

    js2

--- Tema 4/Prueba.js
+++ Tema 4/Prueba.js
@@ -6,10 +6,40 @@ console.log(document);
 console.log(localStorage);
 var esViernes = true;
 console.log('esViernes y queremos irnos: ' + esViernes);
-debugger
 if (esViernes == true) {
     console.log('es viernes, nos vamos de fiesta');
 }
-var nombreUsuario = prompt('introduzca su nombre de usuario: ');
-console.log('el nombre de usuario es: ', nombreUsuario);
-document.write('<h1>' + nombreUsuario + '</h1>');
+
+// var  nombreUsuario = prompt('Introduce su nombre: ');
+
+function comprobarPass() {
+    var pass1 = document.getElementById('pass1').value;
+    var pass2 = document.getElementById('pass2').value;
+    return pass1 == pass2;
+}
+
+function enviarFormulario() {
+    console.log('Llamando a la funciion enviarFormulario');
+    div = document.getElementById("mensajes");
+
+    //las pass coinciden 
+    if(comprobarPass()){
+        var nombre = document.getElementById('nombre').value;
+        div.innerHTML = nombre + "Password OK";
+        div.style.backgroundColor = "green";
+
+        //document.writeln("<h1>" + nombre + "</h1>");
+        //decoment.writeln("<h1>Password OK</h1>");
+    } else{
+        //document.writeln("<h1>Error: Las contraseñas no coinciden </h1>");
+        div.innerHTML = "Error: Las contraseñas no coinciden";
+         div.style.backgroundColor = "red";
+
+    }
+
+}
+
+function cambiaColor(color){
+    var div = document.getElementById("mensajes");
+    div.style.backgroundColor = color;
+}
